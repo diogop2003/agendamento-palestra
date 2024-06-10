98aa3f6be327bf10382efae1055908fbd605edc4
diogo <diogoprado1511@gmail.com>
2024-06-10 17:43:15 -0300

    palestra service criados

---
+++ agendamento-palestra-back/src/palestra/infra/palestra.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreatePalestraDto } from '../dto/create-palestra.dto';
+import { Palestra } from './palestra.entity';
+import { UpdatePalestraDto } from '../dto/update-palestra.dto';
+
+@Injectable()
+export class PalestraService {
+  constructor(
+    @InjectRepository(Palestra)
+    private palestraRepository: Repository<Palestra>,
+  ) {}
+
+  async create(CreatePalestraDto: CreatePalestraDto): Promise<Palestra> {
+    const palestra = this.palestraRepository.create(CreatePalestraDto);
+    return this.palestraRepository.save(palestra);
+  }
+
+  async update(
+    id: number,
+    updatePalestraDto: UpdatePalestraDto,
+  ): Promise<Palestra> {
+    const palestra = await this.palestraRepository.findOne({
+      where: { id },
+    });
+    if (!palestra) {
+      throw new NotFoundException(`Tema com id ${id} não encontrado`);
+    }
+    Object.assign(palestra, updatePalestraDto);
+    return this.palestraRepository.save(palestra);
+  }
+
+  async remove(id: number): Promise<void> {
+    const result = await this.palestraRepository.delete(id);
+    if (result.affected === 0) {
+      throw new NotFoundException(`Palestra com id ${id} não encontrada`);
+    }
+  }
+}
--- agendamento-palestra-back/src/palestra/palestra.module.ts
+++ agendamento-palestra-back/src/palestra/palestra.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PalestraController } from './palestra.controller';
 import { Palestra } from './infra/palestra.entity';
+import { PalestraService } from './infra/palestra.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Palestra])],
-  providers: [],
+  providers: [PalestraService],
   controllers: [PalestraController],
-  exports: [],
+  exports: [PalestraService],
 })
 export class PalestraModule {}
