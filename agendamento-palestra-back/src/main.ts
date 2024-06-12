c6e378a150b9790d036fa707a0d5cfe34f0cd549
diogo <diogoprado1511@gmail.com>
2024-06-12 17:42:30 -0300

    feat docker compose

--- agendamento-palestra-back/src/main.ts
+++ agendamento-palestra-back/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Habilitar CORS
+  app.enableCors({
+    origin: '*', // Você pode especificar seu frontend URL aqui
+  });
+
   await app.listen(3000);
 }
 bootstrap();
