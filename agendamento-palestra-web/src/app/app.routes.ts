b1e0033380a77c6a52a624874d11db169ca7b095
diogo <diogoprado1511@gmail.com>
2024-06-18 10:57:03 -0300

    feat lecture component

--- agendamento-palestra-web/src/app/app.routes.ts
+++ agendamento-palestra-web/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LectureComponent } from './pages/lecture/lecture.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LectureComponent, pathMatch: 'full' },
+    // {
+    //      path: 'logged-area',
+    //     loadChildren: () =>
+    //     import('./logged-area/logged-area.module').then(m => m.LoggedAreaModule),
+    // },
+];
---
+++ agendamento-palestra-web/src/app/pages/pages.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LectureComponent } from './lecture/lecture.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LectureComponent
+  ]
+})
+export class PagesModule { }
