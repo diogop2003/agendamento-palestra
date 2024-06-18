7981631731b4d21af20f9a92c87ad07c105ab42e
diogo <diogoprado1511@gmail.com>
2024-06-18 17:12:43 -0300

    criação do angular com app module

--- agendamento-palestra-web/src/app/app.component.spec.ts
+++ agendamento-palestra-web/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'agendamento-palestra' title`, () => {
+  it(`should have as title 'agendamento-palestra-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('agendamento-palestra');
+    expect(app.title).toEqual('agendamento-palestra-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, agendamento-palestra');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, agendamento-palestra-web');
   });
 });
