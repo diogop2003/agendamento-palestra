fdf158df1f1dd463c28ad2b09887ceffdaf1a2f0
diogo <diogoprado1511@gmail.com>
2024-06-24 17:32:05 -0300

    generate edit theme componente e import

---
+++ agendamento-palestra-web/src/app/pages/theme/modal-edit-theme/modal-edit-theme.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-edit-theme',
+  templateUrl: './modal-edit-theme.component.html',
+  styleUrl: './modal-edit-theme.component.css'
+})
+export class ModalEditThemeComponent {
+
+}
