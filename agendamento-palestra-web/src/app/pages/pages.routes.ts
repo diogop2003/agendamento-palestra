aa5c9c6d443d0bfb17cb291b368a72daeb23f45d
diogo <diogoprado1511@gmail.com>
2024-07-08 16:14:40 -0300

    import error component

--- agendamento-palestra-web/src/app/pages/pages.routes.ts
+++ agendamento-palestra-web/src/app/pages/pages.routes.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LectureComponent } from './lecture/lecture.component';
 import { SpeakerComponent } from './speaker/speaker.component';
 import { ThemeComponent } from './theme/theme.component';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
   { path: '', component: LectureComponent },
   { path: 'palestrante', component: SpeakerComponent },
   { path: 'temas', component: ThemeComponent },
-  { path: '**', component: LectureComponent }
+  { path: '**', component: ErrorComponent }
 ];
 
 @NgModule({
