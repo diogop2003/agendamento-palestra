f7d177e3b154a3b88be1d6303de59e8392a70120
diogo <diogoprado1511@gmail.com>
2024-07-10 18:28:54 -0300

    import e generate directive

--- agendamento-palestra-web/src/app/pages/pages.module.ts
+++ agendamento-palestra-web/src/app/pages/pages.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LectureComponent } from './lecture/lecture.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { PagesRoutesModule } from './pages.routes';
+
+// Componentes
+import { LectureComponent } from './lecture/lecture.component';
 import { SpeakerComponent } from './speaker/speaker.component';
 import { ThemeComponent } from './theme/theme.component';
 import { ModalAddSpeakerComponent } from './speaker/modal-add-speaker/modal-add-speaker.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { ModalAddThemeComponent } from './theme/modal-add-theme/modal-add-theme.component';
 import { ModalEditSpeakerComponent } from './speaker/modal-edit-speaker/modal-edit-speaker.component';
 import { ModalEditThemeComponent } from './theme/modal-edit-theme/modal-edit-theme.component';
@@ -13,6 +15,10 @@ import { ModalAddLectureComponent } from './lecture/modal-add-lecture/modal-add-
 import { ModalEditLectureComponent } from './lecture/modal-edit-lecture/modal-edit-lecture.component';
 import { ModalRemoveLectureComponent } from './lecture/modal-remove-lecture/modal-remove-lecture.component';
 import { ErrorComponent } from './error/error.component';
+
+// Diretiva
+import { CloseButtonDirective } from '../directive/close-button.directive';
+
 @NgModule({
   declarations: [
     LectureComponent,
@@ -25,12 +31,16 @@ import { ErrorComponent } from './error/error.component';
     ModalAddLectureComponent,
     ModalEditLectureComponent,
     ModalRemoveLectureComponent,
-    ErrorComponent
+    ErrorComponent,
+    CloseButtonDirective
   ],
   imports: [
     CommonModule,
     PagesRoutesModule,
     ReactiveFormsModule
+  ],
+  exports: [
+    CloseButtonDirective
   ]
 })
 export class PagesModule { }
