8f77c693fa097c87e5185dedff29713934a6aee2
diogo <diogoprado1511@gmail.com>
2024-07-12 17:57:32 -0300

    generate e import directive

---
+++ agendamento-palestra-web/src/app/directive/text-area-directive.directive.spec.ts
@@ -0,0 +1,8 @@
+import { TextAreaDirectiveDirective } from './text-area-directive.directive';
+
+describe('TextAreaDirectiveDirective', () => {
+  it('should create an instance', () => {
+    const directive = new TextAreaDirectiveDirective();
+    expect(directive).toBeTruthy();
+  });
+});
